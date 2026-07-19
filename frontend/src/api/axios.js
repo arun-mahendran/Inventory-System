7a00b750d3de1c0f891f897c04ef263e97982084
arun-mahendran <b.arunmahendran@gmail.com>
2026-07-19 14:22:35 +0530

    Testing Commit

--- frontend/src/api/axios.js
+++ frontend/src/api/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-    baseURL: import.meta.env.VITE_API_URL,
+    baseURL: "https://inventory-system-backend-50043520954.development.catalystappsail.in",
     headers: {
         "Content-Type": "application/json",
     },
